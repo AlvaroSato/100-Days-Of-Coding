52c621f23d51897fd31030b6317bd24656f0a9a7
AlvaroSato <alvarosato00@gmail.com>
2024-02-16 09:47:47 -0300

    ADD: This was an easy one

---
+++ 01-Begginer-Working With Variables/project.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print('Greetings, Let\'s generate your band name?')
+    cityName = input('What are the city that you grew up?: ')
+    petName = input('What are your pet name?: ')
+    print(f'Your band name could be: {cityName} {petName}')
